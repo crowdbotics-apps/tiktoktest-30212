b5d10fbb70c064eac43829f36083d96d3492e465
Crowdbotics <team@crowdbotics.com>
2021-08-30 23:22:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tiktoktest_30212Identifier",
+  url: "https://tiktoktest_30212Identifier.botics.co",
+  api: "https://tiktoktest_30212Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
